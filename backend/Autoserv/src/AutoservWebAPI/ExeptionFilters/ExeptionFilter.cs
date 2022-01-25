b0b7ffc150b39ae6dbc898266a54266e8274a303
Khrapal Maksym <khrapal_m_g@outlook.com>
2022-01-25 23:25:08 +0100

    Added exeption filters on web and added swagger settings

---
+++ backend/Autoserv/src/AutoservWebAPI/ExeptionFilters/ExeptionFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoservWebAPI.ExeptionFilters
+{
+    /// <summary>
+    /// ExeptionFilter
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ExeptionFilter : ExceptionFilterAttribute
+    {
+        /// <summary>
+        /// Override exeption filter
+        /// </summary>
+        /// <param name="context"></param>
+        public override void OnException(ExceptionContext context)
+        {
+            switch (context.Exception)
+            {
+                case ArgumentException e:
+                    context.Result = new NotFoundObjectResult(e.Message);
+                    break;
+                case InvalidOperationException e:
+                    context.Result = new BadRequestObjectResult(e.Message);
+                    break;
+                case DbUpdateException e:
+                    context.Result = new BadRequestObjectResult($"Does not create new subject. \n Error:{e.InnerException}");
+                    break;
+            }
+        }
+    }
+}
--- backend/Autoserv/src/AutoservWebAPI/Program.cs
+++ backend/Autoserv/src/AutoservWebAPI/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(o =>
+                o.Filters.Add(typeof(ExeptionFilter)));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AlphaCRM.WebAPI",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Khrapal Maksym",
+            Email = "khrapal_m_g@outlook.com",
+            Url = new Uri("https://khrapal-max.github.io/homepage/")
+        },
+    });
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, Assembly.GetExecutingAssembly().GetName().Name + ".xml"));
+});
 
 var app = builder.Build();
 
